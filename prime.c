a31388f81d899c31d0580977f4df6679ba9d9096
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2023-04-03 03:27:17 +0000

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,65 +1,63 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <stdbool.h>
+
 #include "sequent.h"
+
 #include <time.h>
 
 
-
-int *value;
-
+int * value;
 
 bool is_prime(int num) {
-    if (num <= 1) {
-        return false;
+  if (num <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+      return false;
     }
-    
-    for (int i = 2; i <= num / 2; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    
-    return true;
+  }
+
+  return true;
 }
 
+void check() {
 
-
-void check()
-{
-
-if( m_get_myid() ==0){
+  if (m_get_myid() == 0) {
 
     int a;
-printf("\nenter a number to check if prime or not: ");
-scanf("%d", &a);
-value[0] = a;
-m_lock();
-if(is_prime(a)){
-printf("\n%d is prime\n", value[0]);
-}else{
-printf("\n%d is not prime\n", value[0]);
-}
+    printf("\nenter a number to check if prime or not: ");
+    scanf("%d", & a);
+    value[0] = a;
+    m_lock();
+    if (is_prime(a)) {
+      printf("\n%d is prime\n", value[0]);
+    } else {
+      printf("\n%d is not prime\n", value[0]);
+    }
 
+    m_unlock();
+  } else {
+    printf("Process id is not 0 as requested");
 
-m_unlock();
-}else{
-printf("Process id is not 0 as requested");
-  
-}
+  }
 }
 
 int main() {
-    // Generate a random number between 1 and 100
+  // Generate a random number between 1 and 100
 
-    initialize();
-   // srand( (unsigned int) time(NULL));
+  initialize();
+  // srand( (unsigned int) time(NULL));
 
-    value = (int *)shmalloc(sizeof(int));
-     m_set_procs(0);
-    m_fork(check);
-    m_kill_procs();
+  value = (int * ) shmalloc(sizeof(int));
+  m_set_procs(0);
+  m_fork(check);
+  m_kill_procs();
 
-    clean();
-    return 0;
+  clean();
+  return 0;
 }
